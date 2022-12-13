503ea3a2588d88f983646cd2a12c37ebbb68bf68
AlexandrePre <pre.alexandre1@gmail.com>
2022-12-13 22:44:27 +0100

    avec token et protection route

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 
 app.use(express.json());
 
-const { hashPassword } = require("./auth.js");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
+const usersHandlers = require("./usersHandlers");
+const movieHandlers = require("./movieHandlers");
 
 const port = process.env.APP_PORT ?? 5000;
 
@@ -14,30 +16,27 @@ const welcome = (req, res) => {
 };
 
 app.get("/", welcome);
-
-const movieHandlers = require("./movieHandlers");
-
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-
-const usersHandlers = require("./usersHandlers");
-
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
-
-app.post("/api/movies", movieHandlers.postMovie);
-// app.post("/api/users", usersHandlers.postUsers);
-
-
-
-
-
 app.post("/api/users", hashPassword, usersHandlers.postUsers);
+app.post(
+  "/api/login",
+  usersHandlers.getUserByEmailWithPasswordAndPassToNext,verifyPassword
+);
 
+
+app.use(verifyToken);
+
+
+app.post("/api/movies", verifyToken, movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.put("/api/users/:id", usersHandlers.updateUsers);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 app.delete("/api/users/:id", usersHandlers.deleteUsers);
+
+
 
 app.listen(port, (err) => {
   if (err) {
@@ -46,3 +45,21 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
